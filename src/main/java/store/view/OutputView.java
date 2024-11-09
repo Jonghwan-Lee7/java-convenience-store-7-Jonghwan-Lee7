5f749306edf1c2295128e5e42f0a89e000990230
jonghwanlee <kavae550@gmail.com>
2024-11-10 02:06:46 +0900

    feat(view): StockDTO 관련 출력 메서드 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,4 +1,8 @@
 package store.view;
 
+import java.util.List;
+import store.dto.StockDTO;
+
 public interface OutputView {
+    void printStocks(List<StockDTO> stockDTOs);
 }
--- src/main/java/store/view/impl/ConsoleOutputView.java
+++ src/main/java/store/view/impl/ConsoleOutputView.java
@@ -1,6 +1,28 @@
 package store.view.impl;
 
+import java.util.List;
+import store.dto.StockDTO;
 import store.view.OutputView;
 
 public class ConsoleOutputView implements OutputView {
+    private final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
+    private final String NOTICE_CURRENT_STOCKS = "현재 보유하고 있는 상품입니다.";
+
+    @Override
+    public void printStocks(List<StockDTO> stockDTOs){
+        printWelcomeMessage();
+        for (StockDTO stockDTO : stockDTOs) {
+            System.out.println(stockDTO.stock());
+        }
+    }
+
+    private void printWelcomeMessage(){
+        System.out.println(WELCOME_MESSAGE);
+        System.out.println(NOTICE_CURRENT_STOCKS);
+        printSpace();
+    }
+
+    private void printSpace(){
+        System.out.println();
+    }
 }
