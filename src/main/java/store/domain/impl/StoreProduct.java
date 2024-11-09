ef69ce05018ff9b85acbabe5445e64b81d62f562
jonghwanlee <kavae550@gmail.com>
2024-11-09 21:48:38 +0900

    refactor: StoreProduct 필드 추가
    promotionStock을 따로 구분하는게 아니라
    StoreProduct의 필드로 관리

--- src/main/java/store/domain/impl/StoreProduct.java
+++ src/main/java/store/domain/impl/StoreProduct.java
@@ -4,24 +4,38 @@ package store.domain.impl;
 import store.domain.Product;
 
 public class StoreProduct implements Product {
+    // 이름 저장하지 말고 Inventory에서 Set의 키값으로 쓰자.
+    // 그리고 Promotion Inventory를 따로 구현하자.
 
-    private final String name;
     private final int price;
     private int stock;
+    private int promotionStock;
     private final StorePromotion promotion;
 
-    private StoreProduct(String name, int price, int stock, StorePromotion promotion) {
-        this.name = name;
+    private StoreProduct( int price, int stock, int promotionStock, StorePromotion promotion) {
         this.price = price;
+        this.promotionStock = promotionStock;
         this.stock = stock;
         this.promotion = promotion;
     }
 
-    public static StoreProduct create(String name, int price, int stock, StorePromotion promotion) {
-        return new StoreProduct(name, price, stock, promotion);
+    public static StoreProduct create(int price, int stock, int promotionStock, StorePromotion promotion) {
+        return new StoreProduct( price, stock, promotionStock, promotion);
     }
 
-    public void updateStock(int salesCount){
+    public void updateStocks(int salesCount){
         stock -= salesCount;
+    }
+
+    public void addStock(int stock){
+        if(this.stock == 0){
+            this.stock = stock;
+        }
+    }
+
+    public void addPromotionStock(int promotionStock){
+        if(this.promotionStock == 0){
+            this.promotionStock = promotionStock;
+        }
     }
 }
