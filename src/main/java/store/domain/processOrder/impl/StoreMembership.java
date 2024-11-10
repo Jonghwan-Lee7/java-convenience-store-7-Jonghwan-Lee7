bced9d537a020a335abd33aabeb5ba7050322b10
jonghwanlee <kavae550@gmail.com>
2024-11-11 02:12:28 +0900

    feat(domain): StoreMembership 구현

--- src/main/java/store/domain/processOrder/impl/StoreMembership.java
+++ src/main/java/store/domain/processOrder/impl/StoreMembership.java
@@ -1,6 +1,15 @@
 package store.domain.processOrder.impl;
 
+import static java.lang.Math.min;
+
 import store.domain.processOrder.Membership;
 
 public class StoreMembership implements Membership {
+    private final int DISCOUNT_LIMIT = 8000;
+    private final double DISCOUNT_RATE = 0.3;
+
+    @Override
+    public int getDiscountAmount(int amount){
+        return min( DISCOUNT_LIMIT,  (int) Math.floor(amount * DISCOUNT_RATE) );
+    }
 }
